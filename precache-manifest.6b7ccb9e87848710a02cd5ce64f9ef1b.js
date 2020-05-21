a8b9d1afa7b3a7962d609422db7049125f57d1fa
Shivakumara Bannimatti <shivaslb72@gmail.com>
2020-05-21 08:05:00 +0530

    Updates

---
+++ precache-manifest.6b7ccb9e87848710a02cd5ce64f9ef1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daeca7cc68dc218a3001147cf751c88",
+    "url": "/front-end-task/index.html"
+  },
+  {
+    "revision": "a364d28b8f34bf71b059",
+    "url": "/front-end-task/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1f4bec7fd54e49ed9fb",
+    "url": "/front-end-task/static/css/main.8ecf1416.chunk.css"
+  },
+  {
+    "revision": "a364d28b8f34bf71b059",
+    "url": "/front-end-task/static/js/2.6e487380.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/front-end-task/static/js/2.6e487380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f4bec7fd54e49ed9fb",
+    "url": "/front-end-task/static/js/main.cbd62854.chunk.js"
+  },
+  {
+    "revision": "8b0b16dbd61ff2a1a41e",
+    "url": "/front-end-task/static/js/runtime-main.aafe8e72.js"
+  }
+]);
